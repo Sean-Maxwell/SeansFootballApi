2360277295c5e7328b3da61a8be2e90d3090eb5f
x00136016 <x00136016@gmail.com>
2020-08-08 00:28:14 +0100

    Add project files.

---
+++ SeansFootballApi/Controllers/PlayersController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SeansFootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        //Field
+        private IHttpClientFactory httpClient;
+
+        //Ctor (Constructor)
+        public PlayersController(IHttpClientFactory httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        // GET: api/<PlayersController>
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get()
+        {
+            // Create a http GET request to our football API endpoint
+            // api-football-v1.p.rapidapi.com
+            
+            HttpRequestMessage playerRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.footystats.org/league-matches?key=example&league_id=1625");
+
+            // Adding the API key on the header to the request message
+            //test85g57
+           
+
+            //Create an empty HTTP client object
+            HttpClient playerHttpClient = httpClient.CreateClient();
+            //Asynchronously sends the player request message and get the response.
+            var response = await playerHttpClient.SendAsync(playerRequestMessage);
+
+            
+            return response;
+        }
+
+        // GET api/<PlayersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlayersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlayersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlayersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SeansFootballApi/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SeansFootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class ValuesController : ControllerBase
+    {
+        //Field
+        private IHttpClientFactory httpClient;
+
+        public ValuesController(IHttpClientFactory httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get()
+        {
+            // newsapi.org API KEY - 9c5119fef0294cab86be895047a58f00
+            //Insert API into ""
+            HttpRequestMessage newsRequestMessage = new HttpRequestMessage(HttpMethod.Get,
+                "http://newsapi.org/v2/top-headlines?country=us&apiKey=9c5119fef0294cab86be895047a58f00");
+
+            //Creating an empty http client
+            HttpClient playerHttpClient = httpClient.CreateClient();
+
+            var response = await playerHttpClient.SendAsync(newsRequestMessage);
+
+            return response;
+        }
+
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
